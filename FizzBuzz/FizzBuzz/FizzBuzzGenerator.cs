130b907714db3606fc279c3dd1aed870d58f1bb8
charlallison <soprinye@gmail.com>
2018-10-26 11:34:36 +0100

    Added git ignore file and completed FizzBuzz game.

---
+++ FizzBuzz/FizzBuzz/FizzBuzzGenerator.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace FizzBuzz {
+    public class FizzBuzzGenerator {
+
+        public string Generate(int max) {
+            if ((max % 5) == 0 && (max % 3) == 0) return "FizzBuzz";
+
+            if(max.ToString().Contains("3") && max.ToString().Contains("5")) return "FizzBuzz";
+
+            if(max.ToString().Contains("3")) return "Fizz";
+
+            if(max.ToString().Contains("5")) return "Buzz";
+
+            if ((max % 3) == 0) return "Fizz";
+            if ((max % 5) == 0) return "Buzz";
+
+            
+            return max.ToString();
+        }
+
+    }
+}
---
+++ FizzBuzz/FizzBuzzTests/FizzBuzzGeneratorTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Tests {
+    [TestClass()]
+    public class FizzBuzzGeneratorShould {
+
+        FizzBuzzGenerator buzzGenerator;
+
+        [TestInitialize]
+        public void initTest() {
+            buzzGenerator = new FizzBuzzGenerator();
+        }
+
+        [TestMethod()]
+        public void ReturnNumber() {
+            var actual = buzzGenerator.Generate(1);
+            Assert.AreEqual("1", actual);
+        }
+
+        [TestMethod]
+        public void ReturnFizzForMultipleOf3() {
+            var actual = buzzGenerator.Generate(9);
+            Assert.AreEqual("Fizz", actual);
+        }
+
+        [TestMethod]
+        public void ReturnBuzzForMultipleOf5() {
+            var actual = buzzGenerator.Generate(55);
+            Assert.AreEqual("Buzz", actual);
+        }
+
+        [TestMethod]
+        public void ReturnFizzForNumbersThatContain3() {
+            var actual = buzzGenerator.Generate(137);
+            Assert.AreEqual("Fizz", actual);
+        }
+
+        [TestMethod]
+        public void ReturnBuzzForNumberThatContain5() {
+            var actual = buzzGenerator.Generate(51);
+            Assert.AreEqual("Buzz", actual);
+        }
+
+    }
+}
